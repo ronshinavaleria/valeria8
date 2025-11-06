94821a13d166126a3402fcbc87266d0d6838288a
ВАЛЕРИЯ <ronshina.lera0802@gmail.com>
2025-11-06 23:03:14 +0300

    Update pr9.6.py

--- pr9.6.py
+++ pr9.6.py
@@ -14,4 +14,15 @@ if prost_ch(n):
 else:
     print("NO")
 
-
+def find_second_max(pred_max=-1, teku_max=-1):
+    num = int(input())
+    if num == 0:
+        return teku_max
+    if num > pred_max:
+        teku_max = pred_max
+        pred_max = num
+    elif num > teku_max:
+        teku_max = num
+    return find_second_max(pred_max, teku_max)
+result = find_second_max()
+print(result)
